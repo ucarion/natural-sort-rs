b81b843a9afbc1547bc4a93d13dc67e6f3b7227a
Ulysse Carion <ulysse@ulysse.io>
2014-12-06 12:41:57 -0800

    Add support for letters in string.

--- src/natural_sort.rs
+++ src/natural_sort.rs
@@ -1,35 +1,64 @@
 #[deriving(Show, PartialEq, Eq)]
-enum StringElem<'a> {
-    Letters(&'a str),
+enum StringElem {
+    Letters(String),
     Number(int)
 }
 
 #[deriving(Show, PartialEq, Eq)]
-struct NumberSequence<'a> {
-    elems: Vec<StringElem<'a>>
+struct NumberSequence {
+    elems: Vec<StringElem>
 }
 
-impl<'a> NumberSequence<'a> {
-    fn from_str(string: &'a str) -> NumberSequence<'a> {
+impl NumberSequence {
+    pub fn from_str(string: &str) -> NumberSequence {
         let numbers_re = regex!(r"^\p{N}+");
-        let mut elems: Vec<StringElem> = Vec::new();
+        let letters_re = regex!(r"^\P{N}+");
+
+        let mut elems = Vec::new();
         let mut to_parse = String::from_str(string);
 
         while !to_parse.is_empty() {
-            match numbers_re.find(string) {
-                Some((_, end_index)) => {
-                    let prefix_to_num: int =
-                        from_str(to_parse.slice_to(end_index)).unwrap();
-                    elems.push(StringElem::Number(prefix_to_num));
+            let numbers_match = numbers_re.find(to_parse.as_slice());
 
-                    to_parse = to_parse.slice_from(end_index).to_string();
-                },
-                None => {}
+            let (next_token, next_to_parse) = if numbers_match.is_some() {
+                let (_, end_index) = numbers_match.unwrap();
+                process_number(end_index, to_parse, &elems)
+            } else {
+                let letters_match = letters_re.find(to_parse.as_slice());
+                let (_, end_index) = letters_match.unwrap();
+                process_letters(end_index, to_parse, &elems)
             };
+
+            elems.push(next_token);
+            to_parse = next_to_parse;
         }
 
         NumberSequence { elems: elems }
     }
+
+}
+
+fn process_number(end_index: uint,
+                  to_parse: String,
+                  elems: &Vec<StringElem>) -> (StringElem, String) {
+    let prefix_to_num: int = from_str(to_parse.slice_to(end_index))
+                                .unwrap();
+
+    let next_token = StringElem::Number(prefix_to_num);
+    let to_parse_suffix = to_parse.slice_from(end_index).to_string();
+
+    (next_token, to_parse_suffix)
+}
+
+fn process_letters(end_index: uint,
+                   to_parse: String,
+                   elems: &Vec<StringElem>) -> (StringElem, String) {
+    let prefix = to_parse.slice_to(end_index);
+
+    let next_token = StringElem::Letters(prefix.to_string());
+    let to_parse_suffix = to_parse.slice_from(end_index).to_string();
+
+    (next_token, to_parse_suffix)
 }
 
 #[test]
@@ -37,4 +66,10 @@ fn test_makes_numseq() {
     let str1 = "123";
     let seq1 = NumberSequence { elems: vec![StringElem::Number(123)] };
     assert_eq!(NumberSequence::from_str(str1), seq1);
+
+    let str2 = "abc";
+    let seq2 = NumberSequence {
+        elems: vec![StringElem::Letters("abc".to_string())]
+    };
+    assert_eq!(NumberSequence::from_str(str2), seq2);
 }
